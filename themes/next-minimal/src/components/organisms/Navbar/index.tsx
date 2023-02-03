2953fc104e19e3ce1bd81e3919fde5bb2d10108e
hieudoanm <hieumdoan@gmail.com>
2023-02-03 11:46:22 +0700

    add minimal theme

---
+++ themes/next-minimal/src/components/organisms/Navbar/index.tsx
@@ -0,0 +1,38 @@
+import { DarkMode, LightMode } from '@mui/icons-material';
+import React from 'react';
+import content from '../../../content';
+import { useTheme } from '../../../context/ThemeProvider';
+import Link from 'next/link';
+import { Container } from '@mui/material';
+
+export const Navbar: React.FC = () => {
+  const { darkMode, setDarkMode } = useTheme();
+
+  return (
+    <nav className="dark:text-white">
+      <Container>
+        <div className="flex items-center justify-between py-8">
+          <h1 className="text-xl uppercase">{content.seo.title}</h1>
+          <ul className="flex items-center">
+            <li>
+              {!darkMode && (
+                <DarkMode
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-4xl"
+                />
+              )}
+              {darkMode && (
+                <LightMode
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-4xl"
+                />
+              )}
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ themes/next-minimal/src/context/ThemeProvider/index.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode, createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext({
+  darkMode: false,
+  setDarkMode: (darkMode: boolean) => console.log(darkMode),
+});
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
