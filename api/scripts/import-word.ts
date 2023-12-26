9bfe1827b13bb6db2368455a975c2e9b36e23dee
hieudoanm <hieumdoan@gmail.com>
2023-12-26 22:01:24 +0700

    Fix Local

--- api/scripts/import-word.ts
+++ api/scripts/import-word.ts
@@ -13,10 +13,13 @@ const importWord = async (
   wordQuery: string
 ): Promise<void> => {
   try {
+    const baseUrl = `https://www.wordsapi.com/mashape/words/${encodeURIComponent(
+      wordQuery
+    )}`;
     const encrypted = '8cfdb189e722909be89207bfe958babbaeb2290937f892b8';
     const when = '2023-03-23T06:42:24.775Z';
     const limit = 1000000;
-    const url = `https://www.wordsapi.com/mashape/words/${wordQuery}?encrypted=${encrypted}&when=${when}&limit=${limit}`;
+    const url = `${baseUrl}?encrypted=${encrypted}&when=${when}&limit=${limit}`;
     const response = await axios.get<WordDto>(url);
     const { data } = response;
     const { word, results, syllables, pronunciation, frequency } = data;
@@ -28,7 +31,7 @@ const importWord = async (
       where: { word },
     });
   } catch (error) {
-    console.error(`word=${wordQuery} error=${error}`);
+    console.error(`word=${wordQuery} error`);
   }
 };
 
@@ -43,11 +46,10 @@ const main = async () => {
   const wordsSetFromDB = new Set(wordsFromDB);
   console.info(`Filter from DB`);
   const wordsTxt = readFileSync('./scripts/txt/words.txt', 'utf-8');
-  const words = wordsTxt.split('\n').filter((word) => {
-    return !wordsSetFromDB.has(word);
-  });
-  words.reverse();
-  const limit = 1000;
+  const words = wordsTxt
+    .split('\n')
+    .filter((word) => !wordsSetFromDB.has(word));
+  const limit = 100;
   const length = words.length;
   console.info(`length=${length}`);
   const pages = Math.ceil(length / limit);
@@ -55,6 +57,9 @@ const main = async () => {
     const offset = i * limit;
     console.info(`page=${i} offset=${offset} limit=${limit}`);
     const slice: string[] = words.slice(offset, offset + limit);
+    if (slice.length) {
+      continue;
+    }
     await Promise.all(
       slice.map(async (word) => {
         return await importWord(prismaService, word);
