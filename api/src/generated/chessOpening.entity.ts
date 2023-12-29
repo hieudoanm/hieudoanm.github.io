e4d2f3a8e7b6fa36c8366bbc49c7012957e20d46
hieudoanm <hieumdoan@gmail.com>
2023-12-29 16:55:17 +0700

    Add Chess Openings

--- api/src/generated/chessOpening.entity.ts
+++ api/src/generated/chessOpening.entity.ts
@@ -2,9 +2,6 @@ export class ChessOpeningDto {
   eco: string;
   name: string;
   pgn: string;
-  firstMove: string;
-  fen: string;
-  centipawn: number;
   createdAt: Date | null;
   updatedAt: Date | null;
 }
