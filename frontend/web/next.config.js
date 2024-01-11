4f631f3fbc50615891b10648bd68f109b3ad8095
hieudoanm <hieumdoan@gmail.com>
2024-01-11 10:59:33 +0700

    Update CI?CD

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', reactStrictMode: true };
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: { loader: 'akamai', path: '' },
+};
 
 module.exports = nextConfig;
