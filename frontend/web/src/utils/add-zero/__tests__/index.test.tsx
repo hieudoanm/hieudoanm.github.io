723144f3a74d25603c9de15188bd0b158b3b8f1b
hieudoanm <hieumdoan@gmail.com>
2024-01-18 12:31:36 +0700

    Update snapshot

--- frontend/web/src/utils/add-zero/__tests__/index.test.tsx
+++ frontend/web/src/utils/add-zero/__tests__/index.test.tsx
@@ -12,7 +12,7 @@ describe('addZero', () => {
   });
 
   it('add 1 zero for -1', () => {
-    expect(addZero(9, -1)).toEqual('09');
+    expect(addZero(9, -1)).toEqual('9');
     expect(addZero(10, -1)).toEqual('10');
   });
 });
