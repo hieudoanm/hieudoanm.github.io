5bc022541015a9cba9b561acac95d9ca4e427f70
hieudoanm <hieumdoan@gmail.com>
2024-06-21 10:44:35 +0000

    Deploying to gh-pages from @ hieudoanm/hieudoanm.github.io@4afd89cf43dde9568755387d86b683f5ca435d29 🚀

---
+++ _next/static/chunks/pages/apps-ebcf5e15780abb78.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[705],{7184:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/apps",function(){return _(830)}])},830:function(n,u,_){"use strict";_.r(u);var t=_(4246);u.default=()=>(0,t.jsx)(t.Fragment,{})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7184)}),_N_E=n.O()}]);
---
+++ fallback-0gcaMGcRIkomAQ9EWE7Kb.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";self.fallback=async e=>"document"===e.destination?caches.match("/apps",{ignoreSearch:!0}):Response.error()})();
