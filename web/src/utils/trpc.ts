910a186a22e2d97c57e8890748aa5eb6da6fc021
hieudoanm <hieumdoan@gmail.com>
2024-07-16 17:50:19 +0700

    Fix tRPC

--- web/src/utils/trpc.ts
+++ web/src/utils/trpc.ts
@@ -3,19 +3,21 @@ import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/routers/_app';
 
 const getBaseUrl = (): string => {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
+  if (typeof window !== 'undefined') {
+    if (window.location.host.includes('github.io')) {
+      return 'https://hieudoanm.vercel.app';
+    }
+    return ''; // browser should use relative path
+  }
 
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
+  if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
+  }
 
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
+  if (process.env.RENDER_INTERNAL_HOSTNAME) {
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  }
 
-  // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 };
 
