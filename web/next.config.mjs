647074e3f47ae38f850b879d7fff2a94218e47f1
hieudoanm <hieumdoan@gmail.com>
2024-07-17 23:49:37 +0700

    enable cors

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -26,6 +26,27 @@ const nextConfig = {
       ? 'export'
       : 'standalone',
   compiler: { removeConsole: NODE_ENV !== 'development' },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const pwaConfig = withPWA(nextConfig);
