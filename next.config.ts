2ee281bbdd4147b16de2f06f4f92133b0c16c03a
hieudoanm <hieumdoan@gmail.com>
2025-01-21 19:32:18 +0700

    Update Build

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: NODE_ENV === 'development' ? '' : '/clock',
+  basePath: NODE_ENV === 'development' ? '' : '/nothing',
   reactStrictMode: true,
   trailingSlash: true,
   distDir: 'docs',
