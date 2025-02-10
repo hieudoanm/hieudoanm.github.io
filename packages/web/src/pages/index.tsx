3a285c85b5fcbe8dd8e4d77117168b39bdc5daa2
hieudoanm <hieumdoan@gmail.com>
2025-02-10 16:56:56 +0700

    change text

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -5,13 +5,12 @@ const HomePage: NextPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center overflow-hidden bg-gray-100">
       <p className="text-xl font-black lowercase">
-        there is{' '}
+        it is{' '}
         <Link
           href="/apps"
           className="underline decoration-dotted underline-offset-6">
           nothing
-        </Link>{' '}
-        here
+        </Link>
       </p>
     </div>
   );
