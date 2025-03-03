19143dccb1edb6513411f333c0b40132c8a2708f
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-03-03 18:11:21 +0700

    Update Server

--- packages/server/instagram/src/server.ts
+++ packages/server/instagram/src/server.ts
@@ -16,7 +16,7 @@ export const getImages = async (
   // Open Page
   console.info('get.images');
   const browser: Browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     browser: supportedBrowser,
     executablePath,
     headless: true,
