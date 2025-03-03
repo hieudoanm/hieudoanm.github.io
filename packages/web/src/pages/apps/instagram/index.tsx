d6834e3cbb3b8a34a952ddafb49d833e2d056729
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-03-03 20:21:33 +0700

    style instagram

--- packages/web/src/pages/apps/instagram/index.tsx
+++ packages/web/src/pages/apps/instagram/index.tsx
@@ -26,10 +26,13 @@ const InstagramPage: NextPage = () => {
       const headers = {
         'Content-Type': 'application/json', // Specify content type
       };
-      const response = await fetch(
-        'https://nothing-instagram.onrender.com/download',
-        { method: 'POST', headers, body: JSON.stringify({ url }) }
-      );
+      const downloadUrl: string =
+        'https://nothing-instagram.onrender.com/download';
+      const response = await fetch(downloadUrl, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ url }),
+      });
       const data = await response.json();
       const { images } = data;
       setState((previous) => ({
@@ -48,15 +51,15 @@ const InstagramPage: NextPage = () => {
   };
 
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto p-8">
       <div className="flex flex-col gap-y-8">
-        <div className="flex w-full items-center gap-x-4">
+        <div className="flex w-full flex-col items-center gap-4 md:flex-row">
           <input
             type="text"
             id="url"
             name="url"
             placeholder={initialUrl}
-            className="grow rounded border border-gray-300 px-4 py-2"
+            className="w-full grow rounded border border-gray-300 px-4 py-2"
             value={url}
             onChange={(event) => {
               setState((previous) => ({
@@ -67,7 +70,7 @@ const InstagramPage: NextPage = () => {
           />
           <button
             type="button"
-            className="rounded bg-gray-900 px-4 py-2 text-gray-100"
+            className="w-full rounded bg-gray-900 px-4 py-2 text-gray-100 hover:bg-red-500"
             disabled={loading}
             onClick={() => {
               download();
@@ -75,32 +78,49 @@ const InstagramPage: NextPage = () => {
             {loading ? 'Downloading' : 'Download'}
           </button>
         </div>
+        <hr className="border-gray-300" />
         {loading ? <p className="text-center">Loading ...</p> : <></>}
         {!loading && images.length > 0 ? (
-          <div className="grid grid-cols-6 gap-4">
-            {images.map((image: string, index: number) => {
-              return (
-                <div key={image} className="col-span-1">
-                  <div className="flex flex-col gap-y-2">
-                    <div
-                      className="aspect-square w-full rounded border border-gray-300 bg-contain bg-center bg-no-repeat"
-                      style={{ backgroundImage: `url(${image})` }}
-                    />
-                    <button
-                      className="cursor-pointer rounded bg-gray-900 px-4 py-2 text-gray-100 hover:bg-red-500"
-                      onClick={() => {
-                        downloadImage({
-                          format: 'jpg',
-                          content: image,
-                          filename: addZero(index + 1),
-                        });
-                      }}>
-                      Download
-                    </button>
+          <div className="flex flex-col gap-y-4">
+            <button
+              type="button"
+              className="w-full rounded bg-gray-900 px-4 py-2 text-gray-100 hover:bg-red-500"
+              onClick={() => {
+                images.forEach((image: string, index: number) => {
+                  downloadImage({
+                    format: 'jpg',
+                    content: image,
+                    filename: addZero(index + 1),
+                  });
+                });
+              }}>
+              Download All
+            </button>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6">
+              {images.map((image: string, index: number) => {
+                return (
+                  <div key={image} className="col-span-1">
+                    <div className="flex flex-col gap-y-2">
+                      <div
+                        className="aspect-square w-full rounded border border-gray-300 bg-contain bg-center bg-no-repeat"
+                        style={{ backgroundImage: `url(${image})` }}
+                      />
+                      <button
+                        className="cursor-pointer rounded bg-gray-900 px-4 py-2 text-gray-100 hover:bg-red-500"
+                        onClick={() => {
+                          downloadImage({
+                            format: 'jpg',
+                            content: image,
+                            filename: addZero(index + 1),
+                          });
+                        }}>
+                        Download
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         ) : (
           <></>
