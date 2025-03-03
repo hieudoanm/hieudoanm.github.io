ab3ea2f77b992a15699a8420665e2811606b25c3
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-03-03 15:29:11 +0700

    update server

--- packages/server/instagram/src/server.ts
+++ packages/server/instagram/src/server.ts
@@ -129,6 +129,6 @@ const server = createServer((request, response) => {
 const PORT: number = parseInt(process.env.PORT ?? '10000') ?? 10000;
 
 // starts a simple http server locally on port 3000
-server.listen(PORT, '127.0.0.1', () => {
-  console.log(`Listening on 127.0.0.1:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on 0.0.0.0:${PORT}`);
 });
