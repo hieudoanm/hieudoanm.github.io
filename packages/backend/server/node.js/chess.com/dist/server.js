6dadf6b8b1ae5624f86e7599d33061bb47a77c9f
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-03-22 15:04:02 +0700

    Update CI

--- packages/backend/server/node.js/chess.com/dist/server.js
+++ packages/backend/server/node.js/chess.com/dist/server.js
@@ -1,61 +1,32 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const consumer_1 = require('./server/consumer');
-const producer_1 = require('./server/producer');
-const log_1 = require('./utils/log');
-const startServer = () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const consumer_1 = require("./server/consumer");
+const producer_1 = require("./server/producer");
+const log_1 = require("./utils/log");
+const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     (0, producer_1.startProducerServer)();
     yield (0, consumer_1.startConsumerServer)();
-  });
+});
 // Handle graceful shutdown
-process.on('SIGINT', () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+process.on('SIGINT', () => __awaiter(void 0, void 0, void 0, function* () {
     log_1.logger.info('🔄 Shutting down Kafka consumer...');
     yield producer_1.producer.disconnect();
     yield consumer_1.consumer.disconnect();
     process.exit(0);
-  })
-);
-process.on('SIGTERM', () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+}));
+process.on('SIGTERM', () => __awaiter(void 0, void 0, void 0, function* () {
     log_1.logger.info('🔄 Shutting down Kafka consumer...');
     yield producer_1.producer.disconnect();
     yield consumer_1.consumer.disconnect();
     process.exit(0);
-  })
-);
+}));
 startServer();
--- packages/backend/server/node.js/chess.com/dist/server/consumer.js
+++ packages/backend/server/node.js/chess.com/dist/server/consumer.js
@@ -1,79 +1,38 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.startConsumerServer = exports.consumer = void 0;
-const kafka_1 = require('../clients/kafka');
-const constants_1 = require('../constants/constants');
-const chess_service_1 = require('../services/chess.service');
-const log_1 = require('../utils/log');
+const kafka_1 = require("../clients/kafka");
+const constants_1 = require("../constants/constants");
+const chess_service_1 = require("../services/chess.service");
+const log_1 = require("../utils/log");
 exports.consumer = kafka_1.kafka.consumer({ groupId: 'chess-group' });
-const startConsumerServer = () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const startConsumerServer = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-      yield exports.consumer.connect();
-      log_1.logger.info('✅ Kafka Consumer Connected');
-      yield exports.consumer.subscribe({
-        topic: constants_1.TOPIC,
-        fromBeginning: true,
-      });
-      yield exports.consumer.run({
-        eachMessage: (_a) =>
-          __awaiter(
-            void 0,
-            [_a],
-            void 0,
-            function* ({ topic, partition, message }) {
-              var _b, _c, _d;
-              const messageValue =
-                (_c =
-                  (_b = message.value) === null || _b === void 0
-                    ? void 0
-                    : _b.toString()) !== null && _c !== void 0
-                  ? _c
-                  : '{}';
-              const { player = '' } = JSON.parse(messageValue);
-              (0, chess_service_1.addPlayer)(player); // Run Async
-              log_1.logger.info(
-                `📩 Received message: ${topic} ${partition} ${(_d = message.value) === null || _d === void 0 ? void 0 : _d.toString()}`
-              );
-            }
-          ),
-      });
-      log_1.logger.info(`🚀 Kafka Consumer is Running...`);
-    } catch (error) {
-      log_1.logger.error('❌ Kafka Consumer Error:', error);
+        yield exports.consumer.connect();
+        log_1.logger.info('✅ Kafka Consumer Connected');
+        yield exports.consumer.subscribe({ topic: constants_1.TOPIC, fromBeginning: true });
+        yield exports.consumer.run({
+            eachMessage: (_a) => __awaiter(void 0, [_a], void 0, function* ({ topic, partition, message }) {
+                var _b, _c, _d;
+                const messageValue = (_c = (_b = message.value) === null || _b === void 0 ? void 0 : _b.toString()) !== null && _c !== void 0 ? _c : '{}';
+                const { player = '' } = JSON.parse(messageValue);
+                (0, chess_service_1.addPlayer)(player); // Run Async
+                log_1.logger.info(`📩 Received message: ${topic} ${partition} ${(_d = message.value) === null || _d === void 0 ? void 0 : _d.toString()}`);
+            }),
+        });
+        log_1.logger.info(`🚀 Kafka Consumer is Running...`);
     }
-  });
+    catch (error) {
+        log_1.logger.error('❌ Kafka Consumer Error:', error);
+    }
+});
 exports.startConsumerServer = startConsumerServer;
