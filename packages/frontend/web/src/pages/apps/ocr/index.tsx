9baa0bbc59e2684b9e8db4fac7363c572f542aea
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-03-26 12:25:37 +0700

    fix OCR

--- packages/frontend/web/src/pages/apps/ocr/index.tsx
+++ packages/frontend/web/src/pages/apps/ocr/index.tsx
@@ -3,75 +3,64 @@ import Tesseract from 'tesseract.js';
 import { useState } from 'react';
 
 const LicensePlatePage: NextPage = () => {
-  const [{ text = '', imageURL = '', loading = false }, setState] = useState<{
+  const [{ text = '', loading = false }, setState] = useState<{
     text: string;
-    imageURL: string;
     loading: boolean;
   }>({
     text: '',
-    imageURL: '',
     loading: false,
   });
 
   return (
     <div className="h-screen w-screen">
-      <div className="grid h-full grid-cols-2">
-        <div className="col-span-1">
+      <div className="grid h-full grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1">
+        <div className="col-span-1 row-span-1 h-full bg-gray-100 text-gray-900">
           <div className="h-full w-full p-8">
-            <div className="flex flex-col gap-y-4">
-              <label
-                htmlFor="upload-image"
-                className="cursor-pointer border border-dotted px-4 py-2 text-center">
-                <input
-                  type="file"
-                  name="image"
-                  accept="image/png"
-                  id="upload-image"
-                  className="hidden"
-                  onChange={async (event) => {
-                    const files = event.target.files;
-                    if (files === null) return;
-                    const file = files[0];
-                    if (!file) return;
-                    setState((previous) => ({
-                      ...previous,
-                      loading: true,
-                    }));
-                    const imageURL = URL.createObjectURL(file);
-                    Tesseract.recognize(imageURL, 'eng')
-                      .then(({ data }) => {
-                        console.log('data', data);
-                        setState((previous) => ({
-                          ...previous,
-                          imageURL,
-                          text: data.text ?? 'No Text',
-                          loading: false,
-                        }));
-                      })
-                      .catch((error) => {
-                        console.error('error', error);
-                        setState((previous) => ({
-                          ...previous,
-                          imageURL,
-                          text: 'Unable to Recognize',
-                          loading: false,
-                        }));
-                      });
-                  }}
-                />
-                <span>Upload File</span>
-              </label>
-              <div className="aspect-square w-full border border-dotted">
-                <div className="flex h-full w-full items-center justify-center bg-contain bg-center bg-no-repeat">
-                  {imageURL && (
-                    <img src={imageURL} alt="icon" className="w-full" />
-                  )}
-                </div>
-              </div>
-            </div>
+            <label
+              htmlFor="upload-image"
+              className="h-full w-full cursor-pointer border border-dotted text-center">
+              <input
+                type="file"
+                name="image"
+                accept="image/png"
+                id="upload-image"
+                className="hidden"
+                onChange={async (event) => {
+                  const files = event.target.files;
+                  if (files === null) return;
+                  const file = files[0];
+                  if (!file) return;
+                  setState((previous) => ({
+                    ...previous,
+                    loading: true,
+                  }));
+                  const imageURL = URL.createObjectURL(file);
+                  Tesseract.recognize(imageURL, 'eng')
+                    .then(({ data }) => {
+                      console.log('data', data);
+                      setState((previous) => ({
+                        ...previous,
+                        imageURL,
+                        text: data.text ?? 'No Text',
+                        loading: false,
+                      }));
+                    })
+                    .catch((error) => {
+                      console.error('error', error);
+                      setState((previous) => ({
+                        ...previous,
+                        imageURL,
+                        text: 'Unable to Recognize',
+                        loading: false,
+                      }));
+                    });
+                }}
+              />
+              <span>Upload File</span>
+            </label>
           </div>
         </div>
-        <div className="col-span-1">
+        <div className="col-span-1 row-span-1 h-full bg-gray-900 text-gray-100">
           <div className="h-full w-full bg-gray-900 p-8 text-gray-100">
             <p>{loading ? 'Loading' : text}</p>
           </div>
