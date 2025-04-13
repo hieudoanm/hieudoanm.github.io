19b795ef78920db2ebe7f2db56a8803f0ae5aaea
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-04-13 23:52:19 +0700

    update photos

--- packages/frontend/web/next.js/src/pages/apps/photos/gallery/index.tsx
+++ packages/frontend/web/next.js/src/pages/apps/photos/gallery/index.tsx
@@ -1,127 +1,119 @@
+import monet01 from '@web/assets/images/impressionism/monet/01.jpg';
+import monet02 from '@web/assets/images/impressionism/monet/02.jpg';
+import monet03 from '@web/assets/images/impressionism/monet/03.jpg';
+import monet04 from '@web/assets/images/impressionism/monet/04.jpg';
+import monet05 from '@web/assets/images/impressionism/monet/05.jpg';
+import monet06 from '@web/assets/images/impressionism/monet/06.jpg';
+import monet07 from '@web/assets/images/impressionism/monet/07.jpg';
+import vanGogh01 from '@web/assets/images/impressionism/van-gogh/01.jpg';
+import vanGogh02 from '@web/assets/images/impressionism/van-gogh/02.jpg';
+import vanGogh03 from '@web/assets/images/impressionism/van-gogh/03.jpg';
+import vanGogh04 from '@web/assets/images/impressionism/van-gogh/04.jpg';
+import vanGogh05 from '@web/assets/images/impressionism/van-gogh/05.jpg';
+import vanGogh06 from '@web/assets/images/impressionism/van-gogh/06.jpg';
+import kitsch01 from '@web/assets/images/kitsch/01.png';
+import church01 from '@web/assets/images/renaissance/churches/01.jpg';
+import church02 from '@web/assets/images/renaissance/churches/02.jpg';
+import church03 from '@web/assets/images/renaissance/churches/03.jpg';
+import church04 from '@web/assets/images/renaissance/churches/04.jpg';
+import church05 from '@web/assets/images/renaissance/churches/05.jpg';
+import church06 from '@web/assets/images/renaissance/churches/06.jpg';
+import church07 from '@web/assets/images/renaissance/churches/07.jpg';
+import daVinci01 from '@web/assets/images/renaissance/da-vinci/01.jpg';
+import daVinci02 from '@web/assets/images/renaissance/da-vinci/02.jpg';
+import daVinci03 from '@web/assets/images/renaissance/da-vinci/03.jpg';
+import daVinci04 from '@web/assets/images/renaissance/da-vinci/04.jpg';
+import daVinci05 from '@web/assets/images/renaissance/da-vinci/05.jpg';
+import daVinci06 from '@web/assets/images/renaissance/da-vinci/06.jpg';
+import daVinci07 from '@web/assets/images/renaissance/da-vinci/07.jpg';
+import daVinci08 from '@web/assets/images/renaissance/da-vinci/08.jpg';
+import daVinci09 from '@web/assets/images/renaissance/da-vinci/09.jpg';
+import michelangelo01 from '@web/assets/images/renaissance/michelangelo/01.jpg';
+import michelangelo02 from '@web/assets/images/renaissance/michelangelo/02.jpg';
+import michelangelo03 from '@web/assets/images/renaissance/michelangelo/03.jpg';
+import michelangelo04 from '@web/assets/images/renaissance/michelangelo/04.jpg';
+import michelangelo05 from '@web/assets/images/renaissance/michelangelo/05.jpg';
+import michelangelo06 from '@web/assets/images/renaissance/michelangelo/06.jpg';
+import michelangelo07 from '@web/assets/images/renaissance/michelangelo/07.jpg';
+import michelangelo08 from '@web/assets/images/renaissance/michelangelo/08.jpg';
+import michelangelo09 from '@web/assets/images/renaissance/michelangelo/09.jpg';
+import michelangelo10 from '@web/assets/images/renaissance/michelangelo/10.jpg';
+import michelangelo11 from '@web/assets/images/renaissance/michelangelo/11.jpg';
+import michelangelo12 from '@web/assets/images/renaissance/michelangelo/12.jpg';
+import michelangelo13 from '@web/assets/images/renaissance/michelangelo/13.jpg';
+import michelangelo14 from '@web/assets/images/renaissance/michelangelo/14.jpg';
+import michelangelo15 from '@web/assets/images/renaissance/michelangelo/15.jpg';
+import michelangelo16 from '@web/assets/images/renaissance/michelangelo/16.jpg';
+import michelangelo17 from '@web/assets/images/renaissance/michelangelo/17.jpg';
+import michelangelo18 from '@web/assets/images/renaissance/michelangelo/18.jpg';
+import michelangelo19 from '@web/assets/images/renaissance/michelangelo/19.jpg';
+import michelangelo20 from '@web/assets/images/renaissance/michelangelo/20.jpg';
+import michelangelo21 from '@web/assets/images/renaissance/michelangelo/21.jpg';
+import michelangelo22 from '@web/assets/images/renaissance/michelangelo/22.jpg';
+import michelangelo23 from '@web/assets/images/renaissance/michelangelo/23.jpg';
+import michelangelo24 from '@web/assets/images/renaissance/michelangelo/24.jpg';
+import michelangelo25 from '@web/assets/images/renaissance/michelangelo/25.jpg';
+import michelangelo26 from '@web/assets/images/renaissance/michelangelo/26.jpg';
 import { NextPage } from 'next';
-import image01 from '@web/assets/renaissance/01.jpg';
-import image02 from '@web/assets/renaissance/02.jpg';
-import image03 from '@web/assets/renaissance/03.jpg';
-import image04 from '@web/assets/renaissance/04.jpg';
-import image05 from '@web/assets/renaissance/05.jpg';
-import image06 from '@web/assets/renaissance/06.jpg';
-import image07 from '@web/assets/renaissance/07.jpg';
-import image08 from '@web/assets/renaissance/08.jpg';
-import image09 from '@web/assets/renaissance/09.jpg';
-import image10 from '@web/assets/renaissance/10.jpg';
-import image11 from '@web/assets/renaissance/11.jpg';
-import image12 from '@web/assets/renaissance/12.jpg';
-import image13 from '@web/assets/renaissance/13.jpg';
-import image14 from '@web/assets/renaissance/14.jpg';
-import image15 from '@web/assets/renaissance/15.jpg';
-import image16 from '@web/assets/renaissance/16.jpg';
-import image17 from '@web/assets/renaissance/17.jpg';
-import image18 from '@web/assets/renaissance/18.jpg';
-import image19 from '@web/assets/renaissance/19.jpg';
-import image20 from '@web/assets/renaissance/20.jpg';
-import image21 from '@web/assets/renaissance/21.jpg';
-import image22 from '@web/assets/renaissance/22.jpg';
-import image23 from '@web/assets/renaissance/23.jpg';
-import image24 from '@web/assets/renaissance/24.jpg';
-import image25 from '@web/assets/renaissance/25.jpg';
-import image26 from '@web/assets/renaissance/26.jpg';
-import image27 from '@web/assets/renaissance/27.jpg';
-import image28 from '@web/assets/renaissance/28.jpg';
-import image29 from '@web/assets/renaissance/29.jpg';
-import image30 from '@web/assets/renaissance/30.jpg';
-import image31 from '@web/assets/renaissance/31.jpg';
-import image32 from '@web/assets/renaissance/32.jpg';
-import image33 from '@web/assets/renaissance/33.jpg';
-import image34 from '@web/assets/renaissance/34.jpg';
-import image35 from '@web/assets/renaissance/35.jpg';
-import image36 from '@web/assets/renaissance/36.jpg';
-import image37 from '@web/assets/renaissance/37.jpg';
-import image38 from '@web/assets/renaissance/38.jpg';
-import image39 from '@web/assets/renaissance/39.jpg';
-import image40 from '@web/assets/renaissance/40.jpg';
-import image41 from '@web/assets/renaissance/41.jpg';
-import image42 from '@web/assets/renaissance/42.jpg';
-import image43 from '@web/assets/renaissance/43.jpg';
-import image44 from '@web/assets/renaissance/44.jpg';
-import image45 from '@web/assets/renaissance/45.jpg';
-import image46 from '@web/assets/renaissance/46.jpg';
-import image47 from '@web/assets/renaissance/47.jpg';
-import image48 from '@web/assets/renaissance/48.jpg';
-import image49 from '@web/assets/renaissance/49.jpg';
-import image50 from '@web/assets/renaissance/50.jpg';
-import image51 from '@web/assets/renaissance/51.jpg';
-import image52 from '@web/assets/renaissance/52.jpg';
-import image53 from '@web/assets/renaissance/53.jpg';
-import image54 from '@web/assets/renaissance/54.jpg';
-import image55 from '@web/assets/renaissance/55.jpg';
-import image56 from '@web/assets/renaissance/56.jpg';
-import image57 from '@web/assets/renaissance/57.jpg';
-import image58 from '@web/assets/renaissance/58.jpg';
-import image59 from '@web/assets/renaissance/59.jpg';
-import image60 from '@web/assets/renaissance/60.jpg';
 
 const GalleryPage: NextPage = () => {
   const images = [
-    image01,
-    image02,
-    image03,
-    image04,
-    image05,
-    image06,
-    image07,
-    image08,
-    image09,
-    image10,
-    image11,
-    image12,
-    image13,
-    image14,
-    image15,
-    image16,
-    image17,
-    image18,
-    image19,
-    image20,
-    image21,
-    image22,
-    image23,
-    image24,
-    image25,
-    image26,
-    image27,
-    image28,
-    image29,
-    image30,
-    image31,
-    image32,
-    image33,
-    image34,
-    image35,
-    image36,
-    image37,
-    image38,
-    image39,
-    image40,
-    image41,
-    image42,
-    image43,
-    image44,
-    image45,
-    image46,
-    image47,
-    image48,
-    image49,
-    image50,
-    image51,
-    image52,
-    image53,
-    image54,
-    image55,
-    image56,
-    image57,
-    image58,
-    image59,
-    image60,
+    monet01,
+    monet02,
+    monet03,
+    monet04,
+    monet05,
+    monet06,
+    monet07,
+    vanGogh01,
+    vanGogh02,
+    vanGogh03,
+    vanGogh04,
+    vanGogh05,
+    vanGogh06,
+    kitsch01,
+    church01,
+    church02,
+    church03,
+    church04,
+    church05,
+    church06,
+    church07,
+    daVinci01,
+    daVinci02,
+    daVinci03,
+    daVinci04,
+    daVinci05,
+    daVinci06,
+    daVinci07,
+    daVinci08,
+    daVinci09,
+    michelangelo01,
+    michelangelo02,
+    michelangelo03,
+    michelangelo04,
+    michelangelo05,
+    michelangelo06,
+    michelangelo07,
+    michelangelo08,
+    michelangelo09,
+    michelangelo10,
+    michelangelo11,
+    michelangelo12,
+    michelangelo13,
+    michelangelo14,
+    michelangelo15,
+    michelangelo16,
+    michelangelo17,
+    michelangelo18,
+    michelangelo19,
+    michelangelo20,
+    michelangelo21,
+    michelangelo22,
+    michelangelo23,
+    michelangelo24,
+    michelangelo25,
+    michelangelo26,
   ];
 
   return (
