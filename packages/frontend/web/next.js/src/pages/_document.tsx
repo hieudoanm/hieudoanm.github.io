cbeb54fe0759da088cf016d4a8fc5ad432ff78bc
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-04-22 19:49:07 +0700

    Add meta

--- packages/frontend/web/next.js/src/pages/_document.tsx
+++ packages/frontend/web/next.js/src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#101828" />
+        {/* <!-- iOS --> */}
+        <meta name="apple-mobile-web-app-title" content="Hieu Doan" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-touch-fullscreen" content="yes" />
       </Head>
       <body className="bg-gray-900 text-gray-100 antialiased">
         <Main />
