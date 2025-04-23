41530fd979cd2415ceafa914fb02061b9f545110
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-04-23 17:49:48 +0700

    Add ios

---
+++ packages/frontend/mobile/ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Hieu Doan on 23/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
