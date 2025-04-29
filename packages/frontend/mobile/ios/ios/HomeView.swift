3543f2ee2287c06ec9392199d9f9b2c50d72f5e1
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-04-30 01:26:52 +0700

    update notes

--- packages/frontend/mobile/ios/ios/HomeView.swift
+++ packages/frontend/mobile/ios/ios/HomeView.swift
@@ -6,7 +6,10 @@
 //
 import SwiftUI
 
+// GitHub
+let gitHubApp = ""
 let gitHubUrl = "https://github.com/hieudoanm"
+
 let twitterUrl = "https://x.com/hieudoanm"
 let linkedInUrl = "https://www.linkedin.com/in/hieudoanm"
 
@@ -14,19 +17,19 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Hieu Doan")
-                    .padding(.bottom, 16)
+                Text("hieu doan")
+                    .padding(.bottom, 32)
                     .font(.title2)
-                NavigationLink("Apps", destination: AppsView())
-                    .padding(.bottom, 16)
+                NavigationLink("apps", destination: AppsView())
+                    .padding(.bottom, 32)
                     .font(.title2)
-                Link("GitHub", destination: URL(string: gitHubUrl)!)
-                    .padding(.bottom, 8)
+                Link("github", destination: URL(string: gitHubUrl)!)
+                    .padding(.bottom, 2)
                     .font(.title2)
-                Link("Twitter", destination: URL(string: twitterUrl)!)
-                    .padding(.bottom, 8)
+                Link("linkedin", destination: URL(string: linkedInUrl)!)
+                    .padding(.bottom, 2)
                     .font(.title2)
-                Link("LinkedIn", destination: URL(string: linkedInUrl)!)
+                Link("x / twitter", destination: URL(string: twitterUrl)!)
                     .padding(.bottom, 0)
                     .font(.title2)
             }
