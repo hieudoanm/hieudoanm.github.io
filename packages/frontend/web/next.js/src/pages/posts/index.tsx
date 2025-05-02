dbbbe5ecb0d4c2d481119d3098c48e27ecd8b341
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-05-02 17:18:41 +0700

    update notes

--- packages/frontend/web/next.js/src/pages/posts/index.tsx
+++ packages/frontend/web/next.js/src/pages/posts/index.tsx
@@ -37,48 +37,61 @@ const NotesPage: NextPage<{
         </div>
       </div>
       <div className="grow overflow-auto">
-        {posts
-          .filter(({ id = '', title = '' }) => {
-            const tags: string[] = id.split('/').filter((tag) => tag !== '');
-            const tagsFlag: boolean =
-              tags.length > 0 ? tags.includes(search.toLowerCase()) : true;
-            const searchFlag: boolean =
-              search !== ''
-                ? title.toLowerCase().includes(search.toLowerCase())
-                : true;
-            return tagsFlag || searchFlag;
-          })
-          .map(({ id = '', title = '', date = '' }) => {
-            const tags: string[] = id.split('/').filter((tag) => tag !== '');
+        <div className="container mx-auto px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {posts
+              .filter(({ id = '', title = '' }) => {
+                const tags: string[] = id
+                  .split('/')
+                  .filter((tag) => tag !== '');
+                const tagsFlag: boolean =
+                  tags.length > 0 ? tags.includes(search.toLowerCase()) : true;
+                const searchFlag: boolean =
+                  search !== ''
+                    ? title.toLowerCase().includes(search.toLowerCase())
+                    : true;
+                return tagsFlag || searchFlag;
+              })
+              .map(({ id = '', title = '', date = '' }) => {
+                const tags: string[] = id
+                  .split('/')
+                  .filter((tag) => tag !== '');
 
-            return (
-              <div key={id} className="border-b border-gray-800 py-4">
-                <div className="container mx-auto flex flex-col gap-y-2 px-8">
-                  <div className="flex flex-col gap-y-1">
-                    <Link href={`/posts/${id}`}>
-                      <p>
-                        <b>{title}</b>
-                      </p>
-                    </Link>
-                    <small>Date: {date}</small>
+                return (
+                  <div
+                    key={id}
+                    className="col-span-1 border-b border-gray-800 py-4">
+                    <div className="flex flex-col gap-y-2">
+                      <div className="flex flex-col gap-y-1">
+                        <Link href={`/posts/${id}`}>
+                          <p>
+                            <b>{title}</b>
+                          </p>
+                        </Link>
+                        <small>Date: {date}</small>
+                      </div>
+                      <div className="flex flex-wrap items-center gap-2">
+                        {tags.map((tag: string) => (
+                          <button
+                            key={tag}
+                            type="button"
+                            className="inline-block cursor-pointer rounded bg-gray-100 px-1 py-0.5 text-xs text-gray-900"
+                            onClick={() =>
+                              setState((previous) => ({
+                                ...previous,
+                                search: tag,
+                              }))
+                            }>
+                            <b>{tag}</b>
+                          </button>
+                        ))}
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex flex-wrap items-center gap-2">
-                    {tags.map((tag: string) => (
-                      <button
-                        key={tag}
-                        type="button"
-                        className="inline-block cursor-pointer rounded bg-gray-100 px-1 py-0.5 text-xs text-gray-900"
-                        onClick={() =>
-                          setState((previous) => ({ ...previous, search: tag }))
-                        }>
-                        <b>{tag}</b>
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })}
+          </div>
+        </div>
       </div>
     </main>
   );
