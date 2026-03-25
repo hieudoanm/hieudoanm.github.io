dd96a20bcb43df7e3b08ad3c177be77d5f4bd3c3
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 21:26:04 +0700

    Use HeadTemplate

--- docs/_next/static/chunks/a9fefd84e6b2328a.js
+++ docs/_next/static/chunks/a9fefd84e6b2328a.js
@@ -1,9 +1,9 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/1285d1104278028e.js",
   "static/chunks/745a4dabd4b2ee44.js",
-  "static/chunks/43138a09f9eebcb4.js",
   "static/chunks/66ea503cafdf1a37.js",
+  "static/chunks/a44a5e208eec0d19.js",
   "static/chunks/b8d31a628d60a48b.css",
   "static/chunks/d782668ea84e48c1.css",
-  "static/chunks/turbopack-a9fb6994a38961e4.js"
+  "static/chunks/turbopack-e0007d06a7a3d126.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,9 +1,9 @@
+import { HeadTemplate } from '../templates/HeadTemplate';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@web/styles/globals.css';
 import { trpc } from '@web/utils/trpc';
 import type { AppProps } from 'next/app';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Head from 'next/head';
 import { FC, useEffect } from 'react';
 
 const queryClient = new QueryClient();
@@ -32,12 +32,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <>
-      <Head>
-        <title>Hieu Doan</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <HeadTemplate basic={{ title: 'Hieu Doan' }} />
       <div className={`${geistSans.className} ${geistMono.className}`}>
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
