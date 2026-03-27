c13e1890c79d875b1763888be7bb0a225abd36a8
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-27 20:03:00 +0700

    Update Landing Template

--- docs/_next/static/chunks/fcd1ff761d913d91.js
+++ docs/_next/static/chunks/fcd1ff761d913d91.js
@@ -3,7 +3,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/745a4dabd4b2ee44.js",
   "static/chunks/66ea503cafdf1a37.js",
   "static/chunks/a44a5e208eec0d19.js",
-  "static/chunks/b8d31a628d60a48b.css",
+  "static/chunks/4cb72ba1c96bb97a.css",
   "static/chunks/d782668ea84e48c1.css",
-  "static/chunks/turbopack-e0007d06a7a3d126.js"
+  "static/chunks/turbopack-e7b1adeaf1e8fa5b.js"
 ])
