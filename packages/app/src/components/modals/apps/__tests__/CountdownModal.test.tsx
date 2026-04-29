2b7fd0924d898ebe4295c00f9b7d0d3d8f563317
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-04-29 20:40:22 +0700

    Update title

--- packages/app/src/components/modals/apps/__tests__/CountdownModal.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { CountdownModal } from '../CountdownModal';
-
-describe('CountdownModal', () => {
-  it('should render', () => {
-    const { container } = render(<CountdownModal onClose={jest.fn()} />);
-    expect(container).toMatchSnapshot();
-  });
-});
