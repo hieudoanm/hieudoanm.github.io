8ca27922a7da9a353154763a8c8233a7e359c7f7
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-05-03 23:49:55 +0700

    update tests

--- packages/app/src/components/sidebars/LeftSidebar/tabs/__tests__/TimeTab.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { TimeTab } from '../TimeTab';
-
-describe('TimeTab', () => {
-  it('should render', () => {
-    const { container } = render(<TimeTab />);
-    expect(container).toMatchSnapshot();
-  });
-});
