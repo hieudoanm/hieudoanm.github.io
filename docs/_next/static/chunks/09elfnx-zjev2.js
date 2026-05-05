205fe2f1b268abfa210728c3742144cb05755333
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-05-05 23:11:07 +0700

    add battery

---
+++ docs/_next/static/chunks/09elfnx-zjev2.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/app", [
+  "static/chunks/0r55k3hpog17y.js",
+  "static/chunks/0-96p~66zbrz1.js",
+  "static/chunks/0ie9xvksl6_pp.js",
+  "static/chunks/04..m57_d~b5b.js",
+  "static/chunks/0.i6xzj0jfu0m.css",
+  "static/chunks/turbopack-0rlfydwah7ilw.js"
+])
--- packages/modules/browser/react/src/index.ts
+++ packages/modules/browser/react/src/index.ts
@@ -1,2 +1,4 @@
+export * from './battery';
 export * from './clipboard';
+export * from './geolocation';
 export * from './storage';
