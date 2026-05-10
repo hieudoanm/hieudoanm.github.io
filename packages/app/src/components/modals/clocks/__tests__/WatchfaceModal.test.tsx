efaed72af426719b2c53afa9ff5e6b5c277983d8
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-05-10 11:16:23 +0700

    add free models

--- packages/app/src/components/modals/clocks/__tests__/WatchfaceModal.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { WatchFaceModal } from '../WatchfaceModal';
-
-describe('WatchFaceModal', () => {
-  it('should render', () => {
-    const { container } = render(<WatchFaceModal onClose={jest.fn()} />);
-    expect(container).toMatchSnapshot();
-  });
-});
