609b4e9e55a0e09b81f8c86db658510a67068d10
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-06-09 14:18:35 +0700

    update ci

--- packages/app/nextjs.org/next.config.ts
+++ packages/app/nextjs.org/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
 
+const EXPORT: boolean = process.env.EXPORT === 'true';
+
 const nextConfig: NextConfig = {
   trailingSlash: true,
   reactCompiler: true,
   reactStrictMode: true,
+  output: EXPORT ? 'export' : undefined,
 };
 
 export default nextConfig;
