d84a233c3db8e6d8ad65fb2955de85a67473a8e2
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-06-15 10:15:24 +0700

    update cli

--- packages/cli/hieudoanm/swift/Tests/hieudoanmTests/Libs/NumberTests.swift
+++ packages/cli/hieudoanm/swift/Tests/hieudoanmTests/Libs/NumberTests.swift
@@ -1,7 +1,34 @@
 import XCTest
 @testable import hieudoanm
 
-final class EnglishTests: XCTestCase {
+final class NumberTests: XCTestCase {
+    func testAddZero() {
+        XCTAssertEqual(addZero(n: 0), "00")
+        XCTAssertEqual(addZero(n: 1), "01")
+        XCTAssertEqual(addZero(n: 9), "09")
+    }
+
+    func testAddZero_outOfRange() {
+        XCTAssertEqual(addZero(n: 10), "10")
+        XCTAssertEqual(addZero(n: 100), "100")
+        XCTAssertEqual(addZero(n: -1), "-1")
+    }
+
+    func testComma() {
+        XCTAssertEqual(comma(n: 0), "0")
+        XCTAssertEqual(comma(n: 1), "1")
+        XCTAssertEqual(comma(n: 1000), "1,000")
+        XCTAssertEqual(comma(n: 1000000), "1,000,000")
+        XCTAssertEqual(comma(n: 1234567), "1,234,567")
+    }
+
+    func testComma_negative() {
+        XCTAssertEqual(comma(n: -1000), "-1,000")
+        XCTAssertEqual(comma(n: -1234567), "-1,234,567")
+    }
+}
+
+final class NumberEnglishTests: XCTestCase {
     func testNumberToEnglish_zero() {
         XCTAssertEqual(numberToEnglish(0), "zero")
     }
