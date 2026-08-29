86449d40e1349df62214cdcca976a344424349d5
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-08-29 20:33:13 +0700

    Update renovate.json

--- docs/free/chat/_next/static/chunks/3ukdmfaovk5_o.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/33-j6qfa43tra.js",
-  "static/chunks/2mk7cxv2t5w5n.js",
-  "static/chunks/turbopack-2ic8g9h0gc5ov.js"
-])
--- docs/free/football/_next/static/791Tl41bvGsv_DMNY-Qwi/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ftouraments\u002F[tournament]","\u002Ftouraments\u002F[tournament]\u002F[year]","\u002Ftouraments\u002F[tournament]\u002F[year]\u002Fknock-out"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
