e483315200827d1234116d01b7fb2fb1c7f25abc
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-09-07 09:42:59 +0700

    Update Docs Theme

--- packages/app/hybrid/utilities/docs/src/app/layout.tsx
+++ packages/app/hybrid/utilities/docs/src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="nothing">
       <body className={beVietnamPro.className + ' antialiased'}>
         <RootLayoutClient>{children}</RootLayoutClient>
       </body>
